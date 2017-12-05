c02338470849e5f5400b1ca9fd02ae7335d5f3d9
dczheng <dczheng21@outlook.com>
2017-12-06 00:05:21 +0800

    add scipts run_analysis.sh

---
+++ run_analysis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+out_prefix="test_snapshot"
+sed -i '1s/.*/FilePrefix  '"$out_prefix"'/' gadget-analysis.in
+num=`ls ./$out_prefix* | wc -l`
+echo $num
+rm gadget-analysis.log -rf
+mpirun -np $num ./gadget-analysis ./gadget-analysis.in
+
+datas=" mag divB dBdt gas_rho" #rho_0 rho_1" # hge1"
+for i in $datas
+do
+    rm $i.zip
+    zip $i.zip $i -r
+    rm $i -rf
+done
