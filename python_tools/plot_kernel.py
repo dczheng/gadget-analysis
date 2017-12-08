b33789c0c26737111f2111462fee3b7623a57a56
dczheng <dczheng21@outlook.com>
2017-12-08 14:52:01 +0800

    add four kernel function plot

--- python_tools/plot_kernel.py
+++ python_tools/plot_kernel.py
@@ -3,39 +3,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
 
-def k1( q ):
+def k1( q ): # standar kernel
+    norm = 8.0 / np.pi
     if 0 <= q <= 0.5:
-        return 8.0 / np.pi * ( 1 - 6*q*q + 6*q*q*q )
+        return norm * ( 1 - 6*q*q + 6*q*q*q )
     elif q <= 1:
-        return 8.0 / np.pi * ( 2.0 * np.power( 1-q, 3.0 ) )
+        return norm * ( 2.0 * np.power( 1-q, 3.0 ) )
+    else:
+        return 0
+
+def k2( q ): # quinitic kernel
+    u1 = 1 - q
+    u2 = 2/3 - q
+    u3 = 1/3 - q
+    norm = 2187 / ( 40 * np.pi )
+    if 0<= q < 1/3:
+        return (u1**5 - 6*u2**5 + 15*u3**5) * norm
+    elif q < 2/3:
+        return (u1**5 - 6*u2**5) * norm
+    elif q <= 1:
+        return (u1**5) * norm
+    else:
+        return 0
+
+def k3( q ): #wendland c4 kernel
+    norm = 495 / ( 32 * np.pi )
+    u = 1-q
+    if q<=1:
+        return u**6 * ( 1 + 6*q + 35/3*q*q ) * norm
+    else:
+        return 0
+def k4( q ): #wendland c6 kernel
+    norm = 1365 / ( 64 * np.pi )
+    u = 1-q
+    if q<=1:
+        return u**8 * ( 1 + 8*q + 25*q**2 + 32*q**3 ) * norm
     else:
         return 0
 
 fig = plt.figure()
-ax_k1 = fig.add_subplot( 221 )
-ax_k1_2d = fig.add_subplot( 222 )
+ax1 = fig.add_subplot( 221 )
+
+ax2 = fig.add_subplot( 222 )
+
+ax3 = fig.add_subplot( 223 )
+
+ax4 = fig.add_subplot( 224 )
+
+
 N = 100
-x = np.linspace( 0, 1.2, N )
-y = np.zeros( N )
+q = np.linspace( 0, 1, 100 )
+y1 = np.zeros( N )
+y2 = np.zeros( N )
+y3 = np.zeros( N )
+y4 = np.zeros( N )
 for i in range( N ):
-    y[i] = k1( x[i] )
-ax_k1.plot( x, y )
-ax_k1.set_title( 'k1' )
+    y1[i] = k1( q[i] )
+    y2[i] = k2( q[i] )
+    y3[i] = k3( q[i] )
+    y4[i] = k4( q[i] )
+ax1.plot( q, y1 )
+ax1.set_title( "standard kernel" )
+ax2.plot( q, y2 )
+ax2.set_title( "quintic kernel" )
+ax3.plot( q, y3 )
+ax3.set_title( "wendland c4 kernel" )
+ax4.plot( q, y4 )
+ax4.set_title( "wendland c6 kernel" )
 
+f1 = lambda x: k1(x) * x**2
+f2 = lambda x: k2(x) * x**2
+f3 = lambda x: k3(x) * x**2
+f4 = lambda x: k4(x) * x**2
 
-N = 256
-img = np.zeros( (N,N) )
-for i in range( N ):
-    for j in range( N ):
-        x = (j-N/2) / (N/2)
-        y = (i-N/2) / (N/2)
-        r = np.sqrt( x*x + y*y )
-        img[ i, j ] = k1(r)
-k_2d=ax_k1_2d.imshow( img )
-ax_k1_2d.set_title( 'k1_2d' )
-plt.colorbar( k_2d )
+print( quad( f1, 0, 1 )[0] * np.pi * 4 )
+print( quad( f2, 0, 1 )[0] * np.pi * 4 )
+print( quad( f3, 0, 1 )[0] * np.pi * 4 )
+print( quad( f4, 0, 1 )[0] * np.pi * 4 )
+
 plt.show()
-'''
-f = lambda x: k1(x) * x * x
-print( quad( f, 0, 1 )[0] * np.pi * 4 )
-'''
