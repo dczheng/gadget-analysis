e688250e29825cabe79b61ca4c249db2bd1ce104
dczheng <dczheng21@outlook.com>
2018-05-11 21:31:04 +0800

    update

--- g2_bin2hdf5/plot.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import yt
-#ds = yt.load( './snapshot_005.hdf5' )
-ds = yt.load( './out.hdf5' )
-p = yt.ParticlePlot(ds,'particle_position_x','particle_position_y','particle_mass')
-p.save()
