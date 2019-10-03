067f9ae02a2af75d38dfbb8bb8f78de49183d869
dczheng <dczheng21@outlook.com>
2019-10-04 07:31:00 +0800

    update

--- myindent.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#indent -gnu -npsl -npcs -nbs -nsaf -nsai -nsaw -nprs -bap -pmt -l110 *.c
-indent -lp -npsl -cdb -bad -bap -bbb -bbo -br -brs -bs -saw -i4 -l110 -cli4 *.c *.h
--- mywatch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-
-ps aHux | head -1
-ps aHux | grep  gadget-analysis | grep " R"
-set s (ps aHux | grep  gadget-analysis | grep " R" | wc -l)
-
-echo "Threads Num:" $s
